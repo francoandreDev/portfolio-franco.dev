3befb71b509cba24c6cfd270fe9e7d5211f7b7f4
francoandreDev <francoandre.dev@gmail.com>
2022-11-12 01:18:11 -0500

    basic layout

--- src/styles/modules/exports.tsx
+++ src/styles/modules/exports.tsx
@@ -1,7 +1,7 @@
 import appStyles from './App.module.css';
+import footerStyles from "./Footer.module.css";
 import homeStyles from "./Home.module.css";
 import modalStyles from './Modal.module.css';
 import navBarStyles from "./NavBar.module.css";
-import footerStyles from "./Footer.module.css";
 
-export { appStyles, homeStyles, modalStyles, navBarStyles, footerStyles };
+export { appStyles, footerStyles, homeStyles, modalStyles, navBarStyles };
