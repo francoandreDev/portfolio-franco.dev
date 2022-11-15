4ab4215c3b41470c4fcedcd5691f4453ffd0ee12
francoandreDev <francoandre.dev@gmail.com>
2022-11-14 19:56:04 -0500

    update logic of photo transition

--- src/components/presentation/Profile.tsx
+++ src/components/presentation/Profile.tsx
@@ -1,8 +1,6 @@
 import { useSelector } from "react-redux";
 import { profileStyles } from "../../styles/modules/exports";
 import { RootState } from "../../store";
-import profileImage1 from "../../assets/profile-photo.png";
-import profileImage2 from "../../assets/profile-photo-2.png";
 import "../../styles/utils.css";
 
 const Profile = () => {
@@ -11,9 +9,8 @@ const Profile = () => {
         <div className={`${profileStyles["profile-container"]} borders`} style={{
             boxShadow: `0 0 2px 4px var(--color-${theme})`
         }}>
-            <div className={`${profileStyles["line-bright"]}`}></div>
-            <img className={profileStyles["profile-image"]} id={`${profileStyles["profile-image-1"]}`} src={profileImage1} alt="profile-image-1" />
-            <img className = {profileStyles["profile-image"]} id={`${profileStyles["profile-image-2"]}`} src={profileImage2} alt="profile-image-2" />
+            <img className={profileStyles["profile-image"]} id={`${profileStyles["profile-image-1"]}`} alt="profile image 1" />
+            <img className = {profileStyles["profile-image"]} id={`${profileStyles["profile-image-2"]}`} alt="profile image 2" />
         </div>
     );
 };
